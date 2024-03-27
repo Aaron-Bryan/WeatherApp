095245739dd82953fe99138ddecdbec794a9d8e2
Aaron-Bryan <aaronbryanparrilla@gmail.com>
2024-03-27 15:06:30 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+from tkinter import *
+from datetime import datetime
+
+#Initialize tkinter window
+root = Tk()
+root.geometry("400x400")
+root.resizable(0, 0)
+
+root.title("Weather App")
