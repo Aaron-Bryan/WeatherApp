b80c17dc79d966104fe719d97feefa6e4dd2e8af
Aaron-Bryan <aaronbryanparrilla@gmail.com>
2024-03-27 16:28:44 +0800

    Worked on the Main Code

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ root.geometry("400x400")
 root.resizable(0, 0)
 
 root.title("Weather App")
+
+loc_value = StringVar()
+
+#Main Function
+def show_weather():
+    #API Key
+    api_key = "aa4b0f367f2eabc64faf46c61849b120"
+
+    #Get location from user from the input_loc variable
+    loc_name = loc_value.get()
+
+    #URL of the API
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + loc_name + '&appid='+ api_key
